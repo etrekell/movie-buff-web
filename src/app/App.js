4a9b60d9ef09ca5ce3b8c65097db6c57906a8bd9
Ethan Trekell <ethan.trekell@ehi.com>
2022-05-15 17:15:31 -0500

    Changed to use mui for starting page components

--- src/app/App.js
+++ src/app/App.js
@@ -1,22 +1,22 @@
 import * as React from 'react';
-import { MovieBuffLogo } from '../component/MovieBuffLogo';
-import { ThemeProvider } from '@mui/material';
-import { theme } from './MuiAppTheme';
 import CssBaseline from '@mui/material/CssBaseline';
+import { theme } from './MuiAppTheme';
+import { MovieBuffLogo } from '../components/MovieBuffLogo';
+import { ThemeProvider, Typography, Button } from '@mui/material';
 
 export const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <div>
-        <MovieBuffLogo width='200' height='200' />
-        <h1>Movie Buff App</h1>
-        <div>
-          <button onClick={() => alert('Login clicked')}>Login</button>
-        </div>
-        <div>
-          <button onClick={() => alert('Register clicked')}>Register</button>
-        </div>
+        <MovieBuffLogo width='250' height='250' />
+        <Typography variant='h1'>Movie Buff App</Typography>
+        <Button variant='contained' onClick={() => alert('Login clicked')}>
+          Login
+        </Button>
+        <Button variant='outlined' onClick={() => alert('Register clicked')}>
+          Register
+        </Button>
       </div>
     </ThemeProvider>
   );
