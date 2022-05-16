18884abfee5ae744b14e8e04f9278c693332e170
Ethan Trekell <ethan.trekell@ehi.com>
2022-05-16 08:52:44 -0500

    Made a generic LoginDialog

--- src/app/App.js
+++ src/app/App.js
@@ -2,21 +2,12 @@ import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import { theme } from './MuiAppTheme';
 import { MovieBuffLogo } from '../components/MovieBuffLogo';
-import {
-  ThemeProvider,
-  Typography,
-  Button,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-  Stack,
-} from '@mui/material';
+import { ThemeProvider, Typography, Button, Stack } from '@mui/material';
 import { useState } from 'react';
-import { LoginForm } from '../components/LoginForm';
+import { LoginDialog } from '../components/LoginDialog';
 
 export const App = () => {
-  const [openDialog, setOpenDialog] = useState('none');
+  const [showDialog, setShowDialog] = useState('none');
 
   const login = (formData) => {
     console.log('login', formData);
@@ -43,32 +34,26 @@ export const App = () => {
         <MovieBuffLogo width='250' height='250' />
         <Typography variant='h1'>Movie Buff</Typography>
         <Stack spacing={2} direction='row' style={{ padding: '20px' }}>
-          <Button variant='contained' onClick={() => setOpenDialog('login')}>
+          <Button variant='contained' onClick={() => setShowDialog('login')}>
             Login
           </Button>
-          <Button variant='outlined' onClick={() => setOpenDialog('register')}>
+          <Button variant='outlined' onClick={() => setShowDialog('register')}>
             Register
           </Button>
         </Stack>
       </div>
-      <Dialog open={openDialog === 'login'} aria-labelledby='dialog-title'>
-        <DialogActions>
-          <Button onClick={() => setOpenDialog('none')}>Close</Button>
-        </DialogActions>
-        <DialogTitle id='dialog-title'>Login</DialogTitle>
-        <DialogContent>
-          <LoginForm onSubmit={login} buttonText='Login' />
-        </DialogContent>
-      </Dialog>
-      <Dialog open={openDialog === 'register'} aria-labelledby='dialog-title'>
-        <DialogActions>
-          <Button onClick={() => setOpenDialog('none')}>Close</Button>
-        </DialogActions>
-        <DialogTitle id='dialog-title'>Register</DialogTitle>
-        <DialogContent>
-          <LoginForm onSubmit={register} buttonText='Register' />
-        </DialogContent>
-      </Dialog>
+      <LoginDialog
+        dialogType='login'
+        showDialog={showDialog === 'login'}
+        setOpenDialog={setShowDialog}
+        handleSubmit={login}
+      />
+      <LoginDialog
+        dialogType='register'
+        showDialog={showDialog === 'register'}
+        setOpenDialog={setShowDialog}
+        handleSubmit={register}
+      />
     </ThemeProvider>
   );
 };
