1aac94fe1c47e080598f89a4a390cd4182135027
Ethan Trekell <ethan.trekell@ehi.com>
2022-05-15 19:06:37 -0500

    Set up the login and registration dialogs and forms

--- src/app/App.js
+++ src/app/App.js
@@ -2,21 +2,51 @@ import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import { theme } from './MuiAppTheme';
 import { MovieBuffLogo } from '../components/MovieBuffLogo';
-import { ThemeProvider, Typography, Button } from '@mui/material';
+import { ThemeProvider, Typography, Button, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
+import { useState } from 'react';
+import { LoginForm } from '../components/LoginForm';
 
 export const App = () => {
+  const [openDialog, setOpenDialog] = useState('none');
+
+  const login = (formData) => {
+    console.log('login', formData);
+  };
+
+  const register = (formData) => {
+    console.log('register', formData);
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <div>
         <MovieBuffLogo width='250' height='250' />
         <Typography variant='h1'>Movie Buff App</Typography>
-        <Button variant='contained' onClick={() => alert('Login clicked')}>
+        <Button variant='contained' onClick={() => setOpenDialog('login')}>
           Login
         </Button>
-        <Button variant='outlined' onClick={() => alert('Register clicked')}>
+        <Button variant='outlined' onClick={() => setOpenDialog('register')}>
           Register
         </Button>
+        <Dialog open={openDialog === 'login'} aria-labelledby='dialog-title'>
+          <DialogActions>
+            <Button onClick={() => setOpenDialog('none')}>Close</Button>
+          </DialogActions>
+          <DialogTitle id='dialog-title'>Login Form</DialogTitle>
+          <DialogContent>
+            <LoginForm onSubmit={login} buttonText='Login' />
+          </DialogContent>
+        </Dialog>
+        <Dialog open={openDialog === 'register'} aria-labelledby='dialog-title'>
+          <DialogTitle id='dialog-title'>Registration Form</DialogTitle>
+          <DialogContent>
+            <LoginForm onSubmit={register} buttonText='Register' />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => setOpenDialog('none')}>Close</Button>
+          </DialogActions>
+        </Dialog>
       </div>
     </ThemeProvider>
   );
---
+++ src/components/LoginForm.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Button } from '@mui/material';
+
+export const LoginForm = ({ onSubmit, buttonText }) => {
+  // Note: Whenever you make a form, the first thing you should do is make a handleSubmit method that prevents the default.
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { username, password } = e.target.elements;
+
+    onSubmit({ username: username.value, password: password.value });
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor='username'>Username: </label>
+          <input id='username' type='text' />
+        </div>
+        <div>
+          <label htmlFor='password'>Password: </label>
+          <input id='password' type='password' />
+        </div>
+        <div>
+          <Button type='submit' variant='outlined'>
+            {buttonText}
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
