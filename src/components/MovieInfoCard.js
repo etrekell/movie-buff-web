fb47e7dd185630e18adb7756696c2eaf7a7ce300
Ethan Trekell <ethan.trekell@ehi.com>
2022-05-17 18:08:22 -0500

    Made a MovieInfoCard

---
+++ src/components/MovieInfoCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Box, Card, CardContent, Typography, CardMedia } from '@mui/material';
+
+export const MovieInfoCard = ({ title, overview, poster_path }) => {
+  return (
+    <Box width='300px'>
+      <Card>
+        <CardMedia component='img' image={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={`${title} poster`} />
+        <CardContent>
+          <Typography gutterBottom variant='h5' component='div'>
+            {title}
+          </Typography>
+          <Typography variant='body2' color='text.secondary'>
+            {overview}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Box>
+  );
+};
