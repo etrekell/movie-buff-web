fa24494e1049553f05c336fabe341e6899222b2c
Ethan Trekell <ethan.trekell@ehi.com>
2022-05-18 11:49:40 -0500

    Added an image to use as a poster fallback

--- src/components/MovieInfoCard.js
+++ src/components/MovieInfoCard.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Box, Card, CardContent, Typography, CardMedia } from '@mui/material';
+import noPosterAvail from './../images/no-poster-avail.png';
 
 export const MovieInfoCard = ({ title, overview, poster_path }) => {
+  const poster = poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : noPosterAvail;
+  const posterAlt = poster_path ? `${title} poster` : 'No poster available';
+
   return (
     <Box width='500px'>
       <Card>
-        <CardMedia component='img' image={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={`${title} poster`} />
+        <CardMedia component='img' image={poster} alt={posterAlt} />
         <CardContent>
           <Typography gutterBottom variant='h5' component='div'>
             {title}
