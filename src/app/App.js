2950180b25b2bccee7589fc6a67d0ee04f84d387
Ethan Trekell <ethan.trekell@ehi.com>
2022-06-30 18:15:10 -0500

    Made an authStateChanged method to get a promise and solved reloading and user name problems

--- src/app/App.js
+++ src/app/App.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { auth } from '../firebaseConfig';
 import { theme } from './MuiAppTheme';
 import { ThemeProvider } from '@mui/material';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { UnauthenticatedApp } from '../components/UnauthenticatedApp';
 import {
   createUserWithEmailAndPassword,
@@ -12,19 +12,29 @@ import {
   onAuthStateChanged,
 } from 'firebase/auth';
 import { AuthenticatedApp } from '../components/AuthenticatedApp';
+import { useAsync } from '../utilities/hooks/useAsync';
+
+const getAuthStateChangedPromise = async () => {
+  return new Promise((resolve, reject) => {
+    onAuthStateChanged(
+      auth,
+      (user) => {
+        resolve(user);
+      },
+      (error) => {
+        reject(error);
+      }
+    );
+  });
+};
 
 export const App = () => {
-  const [user, setUser] = useState(null);
+  const { data: user, setData: setUser, run, isLoading, isIdle, isError, isSuccess } = useAsync();
 
   // Without this, the user is logged out on every refresh
   useEffect(() => {
-    onAuthStateChanged(auth, (authUser) => {
-      if (authUser) {
-        setUser(authUser);
-      }
-    });
-    // This empty array means that it happens on every onMount
-  }, []);
+    run(getAuthStateChangedPromise());
+  }, [run]);
 
   const login = (formData) => {
     signInWithEmailAndPassword(auth, formData.email, formData.password).then(({ user }) => {
@@ -46,13 +56,25 @@ export const App = () => {
     signOut(auth).then(() => setUser(null));
   };
 
-  return (
-    <ThemeProvider theme={theme}>
-      {user ? (
-        <AuthenticatedApp user={user} logout={logout} />
-      ) : (
-        <UnauthenticatedApp login={login} register={register} />
-      )}
-    </ThemeProvider>
-  );
+  if (isLoading || isIdle) {
+    // TODO: replace with a fancy spinner
+    return <h1>LOADING...</h1>;
+  }
+
+  if (isError) {
+    // TODO: replace with something better
+    return <h1>ERROR</h1>;
+  }
+
+  if (isSuccess) {
+    return (
+      <ThemeProvider theme={theme}>
+        {user ? (
+          <AuthenticatedApp user={user} logout={logout} />
+        ) : (
+          <UnauthenticatedApp login={login} register={register} />
+        )}
+      </ThemeProvider>
+    );
+  }
 };
