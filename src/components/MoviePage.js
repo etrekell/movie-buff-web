ec01561ab18a8a6877b02f607f6baac786673551
Ethan Trekell <ethan.trekell@ehi.com>
2022-07-05 16:54:57 -0500

    Build out MoviePage shell

--- src/components/MoviePage.js
+++ src/components/MoviePage.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Typography } from '@mui/material';
-import { centeredStyle } from './lib';
+import { centeredStyle, FullPageLoadingSpinner, SomethingsWrongError } from './lib';
 import { useParams } from 'react-router-dom';
+import { useAsync } from '../utilities/hooks/useAsync';
+import { customFetch } from '../utilities/customFetch';
 
 export const MoviePage = () => {
+  const { run, data, error, isLoading, isSuccess, isIdle } = useAsync();
   // This allows us to pull the query params from the url out into variables
   const { movieId } = useParams();
 
-  return (
-    // TODO: make this look good
-    <div style={centeredStyle}>
-      <Typography>MOVIE PAGE PLACEHOLDER</Typography>
-      <Typography>Going to change this to make it look better. Make the tmdb call with this movie id:</Typography>
-      <Typography>{movieId}</Typography>
-    </div>
-  );
+  useEffect(() => {
+    const tmdbMoviePath = `${process.env.REACT_APP_TMDB_API_BASE_URL}/movie/${movieId}?api_key=${process.env.REACT_APP_TMDB_API_KEY}`;
+    run(customFetch(tmdbMoviePath));
+  }, [run, movieId]);
+
+  const getStatusBasedComponent = () => {
+    if (isLoading || isIdle) {
+      return <FullPageLoadingSpinner />;
+    }
+
+    if (isSuccess) {
+      return (
+        <div style={centeredStyle}>
+          {data?.original_title ? <Typography>Title: {data.original_title}</Typography> : null}
+        </div>
+      );
+    }
+    // if isError or anything else (Should never be anything else at this point but adding as a fallback)
+    return <SomethingsWrongError errorMessage={error ? error.message : null} />;
+  };
+
+  return getStatusBasedComponent();
 };
