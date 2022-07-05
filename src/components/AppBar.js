97bbf9150b4f25140da1cfacf948af5d83d9a47f
Ethan Trekell <ethan.trekell@ehi.com>
2022-07-05 13:58:56 -0500

    Changed color of the AppBar

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { AppBar as MuiAppBar, Button, IconButton, Stack, Toolbar, Typography } from '@mui/material';
 import { MovieBuffLogo } from './MovieBuffLogo';
 import { NavButton } from './lib';
+import { theme } from '../app/MuiAppTheme';
+import { useNavigate } from 'react-router-dom';
 
 export const AppBar = ({ user, logout }) => {
+  const navigate = useNavigate();
+
   // TODO: Do something better here (Like the dropdown to either login or logout)
   const displayName = user ? user.displayName : '????';
-
   return (
-    <MuiAppBar position='static'>
+    <MuiAppBar position='static' sx={{ background: theme.palette.primary.dark }}>
       <Toolbar variant='dense'>
-        <IconButton size='small' edge='start' aria-label='logo'>
+        <IconButton size='small' edge='start' aria-label='logo' onClick={() => navigate('/discover')}>
           <MovieBuffLogo width='25' height='25' />
         </IconButton>
         <Typography color='inherit' variant='h6' component='div' sx={{ flexGrow: 1, paddingLeft: '10px' }}>
